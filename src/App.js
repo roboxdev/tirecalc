f3e6a19110585a9196295ea35915b2751f435e3e
robox <root@roboxv.pro>
2018-11-09 12:30:45 +0600

    Init

--- src/App.js
+++ src/App.js
@@ -1,28 +1,234 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import fromPairs from 'lodash/fromPairs';
+import uniq from 'lodash/uniq';
+import groupBy from 'lodash/groupBy';
+import styled from 'styled-components';
+import cartesian from './utils/cartesian';
+import {
+  oneKmInMm,
+  WIDTH_OPTIONS,
+  HEIGHT_OPTIONS,
+  DIAMETER_OPTIONS,
+  TABLE_DISPLAY_OPTIONS,
+  COLORS,
+} from './constants';
 import './App.css';
 
+const DiffCell = ({ data: {diffScore, diff, diffPercent} = {} }) => (
+  <CellStyled diffScore={diffScore}>
+    {diff.toFixed(2)} ({ Math.abs(diffPercent.toFixed())}%)
+    </CellStyled>
+);
+
+const RideHeightDiffCell = ({ data: {diffScore, rideHeightDiff} = {} }) => (
+  <CellStyled diffScore={diffScore}>
+    {rideHeightDiff.toFixed()}
+    </CellStyled>
+);
+
 class App extends Component {
+  state = {
+    referenceWidth: 185,
+    referenceHeight: 75,
+    referenceDiameter: 16,
+    newDiameter: 16,
+
+    minWidthIndex: 0,
+    maxWidthIndex: WIDTH_OPTIONS.length - 1,
+
+    table: {},
+    tableDisplay: 'diff',
+  };
+
+  componentDidMount() {
+    this.refreshTable();
+  };
+
+  calculate = ({ width, height, diameter }) => {
+    const rimDiameter = diameter * 25.4;
+    const tireHeight = width * height / 100;
+    const tireDiameter = tireHeight * 2 + rimDiameter;
+    const circumference = tireDiameter * Math.PI;
+    const revsPerKm = oneKmInMm / circumference;
+    return {
+      width,
+      height,
+      diameter,
+      rimDiameter,
+      tireHeight,
+      circumference,
+      tireDiameter,
+      revsPerKm,
+    };
+  };
+
+  getReference = () => {
+    const { referenceWidth, referenceHeight, referenceDiameter } = this.state;
+    return this.calculate({ width: referenceWidth, height: referenceHeight, diameter: referenceDiameter });
+  };
+
+  refreshTable = () => {
+    const { minWidthIndex, maxWidthIndex } = this.state;
+    const reference = this.getReference();
+    const SELECTED_WIDTH_OPTIONS = WIDTH_OPTIONS.slice(minWidthIndex, maxWidthIndex + 1);
+    const product = cartesian(SELECTED_WIDTH_OPTIONS, HEIGHT_OPTIONS, DIAMETER_OPTIONS);
+    const table = fromPairs(product.map(([width, height, diameter]) => {
+      const {
+        rimDiameter,
+        tireHeight,
+        circumference,
+        tireDiameter,
+        revsPerKm,
+      } = this.calculate({ width, height, diameter });
+      const diff = tireDiameter - reference.tireDiameter;
+      const diffPercent = (1 - tireDiameter / reference.tireDiameter) * 100;
+      const rideHeightDiff = diff / 2;
+      const diffScore = Math.abs(diffPercent.toFixed());
+      const key = `${width}/${height}/R${diameter}`;
+      return [
+        key,
+        {
+          key,
+          width,
+          height,
+          diameter,
+          rimDiameter,
+          tireHeight,
+          circumference,
+          tireDiameter,
+          revsPerKm,
+          diff,
+          diffPercent,
+          rideHeightDiff,
+          diffScore,
+        }];
+    }));
+    this.setState({ table });
+  };
+
+  getList = () => {
+    const { table, referenceDiameter } = this.state;
+    const a = groupBy(Object.values(table).filter(({ diameter, diffScore }) => referenceDiameter === diameter && diffScore <= 3), ({ diffScore }) => Math.max(diffScore, 1));
+    console.log(a);
+    return a;
+  };
+
+  getGoodDiameters = () => uniq(Object.values(this.state.table).filter(({ diffScore }) => diffScore <= 3).map(({ diameter }) => diameter)).sort();
+
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({[name]: value});
+  };
+
+  handleReferenceChange = e => {
+    const { name, value } = e.target;
+    this.setState(
+      {
+        [name]: value,
+      },
+      this.refreshTable
+    );
+  };
+
+  handleDiameterChange = e => {
+    const { name, value } = e.target;
+    this.setState(
+      {
+        [name]: value,
+        newDiameter: value,
+      },
+      this.refreshTable
+    );
+  };
+
   render() {
+    const { referenceWidth, referenceHeight, referenceDiameter, newDiameter, table, tableDisplay } = this.state;
+    const cellMapping = {
+      diff: DiffCell,
+      rideHeightDiff: RideHeightDiffCell,
+    };
+    const Cell = cellMapping[tableDisplay];
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div>
+          Эталон
+          <select value={referenceWidth} name="width" onChange={this.handleReferenceChange}>
+            {WIDTH_OPTIONS.map(width =>
+              <option key={width} value={width}>{width}</option>
+            )}
+          </select> /
+          <select value={referenceHeight} name="height" onChange={this.handleReferenceChange}>
+            {HEIGHT_OPTIONS.map(height =>
+              <option key={height} value={height}>{height}</option>
+            )}
+          </select> /
+          R<select value={referenceDiameter} name="diameter" onChange={this.handleDiameterChange}>
+            {DIAMETER_OPTIONS.map(diameter =>
+              <option key={diameter} value={diameter}>{diameter}</option>
+            )}
+          </select>
+        </div>
+        <div>
+          <select value={tableDisplay} name="tableDisplay" onChange={this.handleChange}>
+            {TABLE_DISPLAY_OPTIONS.map(([option, label]) =>
+              <option key={option} value={option}>{label}</option>
+            )}
+          </select>
+        </div>
+        {Object.keys(table).length && <><table>
+          <thead>
+            <tr>
+              <th>
+                R<select value={newDiameter} name="newDiameter" onChange={this.handleChange}>
+                {this.getGoodDiameters().map(diameter =>
+                  <option key={diameter} value={diameter}>{diameter}</option>
+                )}
+              </select>
+              </th>
+              {HEIGHT_OPTIONS.map(height => <th key={`col-header-${height}`}>{height}</th>)}
+            </tr>
+          </thead>
+          <tbody>
+            {WIDTH_OPTIONS.map(width => <tr key={`row-header-${width}`}>
+              <td>{width}</td>
+              {
+                HEIGHT_OPTIONS.map(
+                  height => <Cell
+                    key={`${width}/${height}/R${newDiameter}`}
+                    data={table[`${width}/${height}/R${newDiameter}`]}
+                  />
+                )
+              }
+            </tr>)}
+          </tbody>
+        </table>
+        { Object.entries(this.getList()).sort(([k]) => k).map(([diffScore, tires]) => <ul key={diffScore}>
+          {tires.map(({ key, diffScore }) => <TireItemStyled key={key} diffScore={diffScore}>{key}</TireItemStyled>)}
+        </ul>) }
+
+        </>}
+
+
       </div>
     );
   }
 }
 
+const CellStyled = styled.td`
+  background-color: ${p => ({
+  [true]: 'none',
+  [p.diffScore <= 3]: COLORS.meh,
+  [p.diffScore <= 2]: COLORS.good,
+  [p.diffScore <= 1]: COLORS.best,
+}.true)};
+`;
+
+const TireItemStyled = styled.li`
+  background-color: ${p => ({
+  [true]: 'none',
+  [p.diffScore <= 3]: COLORS.meh,
+  [p.diffScore <= 2]: COLORS.good,
+  [p.diffScore <= 1]: COLORS.best,
+}.true)};
+`
 export default App;
---
+++ src/constants.js
@@ -0,0 +1,78 @@
+export const oneKmInMm = 1000 * 1000;
+
+export const WIDTH_OPTIONS = [
+  135,
+  145,
+  155,
+  165,
+  175,
+  185,
+  195,
+  205,
+  215,
+  225,
+  235,
+  245,
+  255,
+  265,
+  275,
+  285,
+  295,
+  305,
+  315,
+  325,
+  335,
+  345,
+  355,
+  365,
+  375,
+  385,
+  395,
+];
+
+export const HEIGHT_OPTIONS = [
+  25,
+  30,
+  35,
+  40,
+  45,
+  50,
+  55,
+  60,
+  65,
+  70,
+  75,
+  80,
+  85,
+  90,
+  95,
+  100,
+];
+
+export const DIAMETER_OPTIONS = [
+  12,
+  13,
+  14,
+  15,
+  16,
+  17,
+  18,
+  19,
+  20,
+  21,
+  22,
+  22.5,
+  23,
+  24,
+];
+
+export const TABLE_DISPLAY_OPTIONS = [
+  ['diff', 'Разница диаметров'],
+  ['rideHeightDiff', 'Разница в дорожном просвете'],
+];
+
+export const COLORS = {
+  meh: '#e6ffe6',
+  good: '#99ff99',
+  best: '#33FF33',
+};
